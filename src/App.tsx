57bc55e14a4926cccbaeebf811c82f863ccf1a5a
OsuwoJr <johnfredosuwo@gmail.com>
2024-07-11 05:35:21 +0300

    type variables and returning types

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
+//import React from 'react'
 import './App.css'
 
 const App = () => {
   const name:string = "Osuwo"
   const age:number = 26
   const isMarried:boolean = false
-  
+  const getName = (name:string): number=>{
+    if (name==="Pedro"){
+      return 28
+    }else {
+      return 0
+    }
+
+  }
   return (
     <div className='App'>
       {name}
       {age}
       {isMarried}
+     
     </div>
   )
+  getName
 }
 
 export default App
