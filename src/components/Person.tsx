153e7b3f5745d90c14c91ba71cc079ebba65cc9b
OsuwoJr <johnfredosuwo@gmail.com>
2024-07-12 05:16:22 +0300

    Create Person.tsx
    How to create props

---
+++ src/components/Person.tsx
@@ -0,0 +1,16 @@
+
+interface Props {
+  name: string;
+  age: number;
+  email:string;
+}
+
+export const Person = (props: Props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+      <h1>{props.email}</h1>
+    </div>
+  );
+}
