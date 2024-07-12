d195067ece4e4b8f0a4973c0fb9d0835d1ecba70
OsuwoJr <johnfredosuwo@gmail.com>
2024-07-12 05:52:23 +0300

    how to call states and give them types

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -13,6 +13,8 @@ export const Person:FC<Props> = ({name,age,email}: Props) => {
       <h1>{name}</h1>
       <h1>{age}</h1>
       <h1>{email}</h1>
+      <input placeholder="Write down your country..." onChange={(event:React.ChangeEvent<HTMLInputElement>) => {setCountry(event.target.value)}}/>
+      {country}
     </div>
   );
 }
