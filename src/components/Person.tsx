8af0177c27dbaeeac1f10948dbeb4c51bd987979
OsuwoJr <johnfredosuwo@gmail.com>
2024-07-12 05:29:26 +0300

    Other way of passing props

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -5,12 +5,12 @@ interface Props {
   email ?:string;
 }
 
-export const Person = (props: Props) => {
+export const Person = ({name,age,email}: Props) => {
   return (
     <div>
-      <h1>{props.name}</h1>
-      <h1>{props.age}</h1>
-      <h1>{props.email}</h1>
+      <h1>{name}</h1>
+      <h1>{age}</h1>
+      <h1>{email}</h1>
     </div>
   );
 }
