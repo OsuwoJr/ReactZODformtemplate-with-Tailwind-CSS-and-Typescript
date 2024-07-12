a23e5340fec9d9979fd9a93c79decb7400006f62
OsuwoJr <johnfredosuwo@gmail.com>
2024-07-12 10:03:59 +0300

    Context API

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-//import React from 'react'
+import {createContext} from 'react'
 import './App.css'
 import { Person,BoysLuck } from './components/Person'
 
+interface AppContextInterface {
+  name: string;
+  age: number;
+  country:string;
+}
+
+const AppContext = createContext<AppContextInterface | null>(null)
+
 const App = () => {
+
+  const contextValue:AppContextInterface = {
+    name:"John",
+    age:20,
+    country:"Brazil"
+  }
  
   return (
-    <div className='App'>
+    <AppContext.Provider value={contextValue}>
+      <div className='App'>
       <Person name="Osuwo" age={20} email="osuwoodongo@gmail.com" boysLuck={BoysLuck.Short}/>
+      
     </div>
+    </AppContext.Provider>
+    
   )
  
 }
