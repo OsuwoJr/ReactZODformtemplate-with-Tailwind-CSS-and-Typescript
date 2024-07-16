89595788f223b0399319be8bc76ea721fd6f3072
OsuwoJr <johnfredosuwo@gmail.com>
2024-07-16 11:35:06 +0300

    Create ZOD and hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
-import {createContext} from 'react'
-import './App.css'
-import { Person,BoysLuck } from './components/Person'
-
-interface AppContextInterface {
-  name: string;
-  age: number;
-  country:string;
+import "./App.css"
+export interface IAppProps {
 }
 
-const AppContext = createContext<AppContextInterface | null>(null)
-
-const App = () => {
-
-  const contextValue:AppContextInterface = {
-    name:"John",
-    age:20,
-    country:"Brazil"
-  }
- 
+function App() {
   return (
-    <AppContext.Provider value={contextValue}>
-      <div className='App'>
-      <Person name="Osuwo" age={20} email="osuwoodongo@gmail.com" boysLuck={BoysLuck.Short}/>
+    <div className="App">
+      <form>
+        <label>
+
+        </label>
+      </form>
       
     </div>
-    </AppContext.Provider>
-    
-  )
- 
+  );
 }
 
 export default App
