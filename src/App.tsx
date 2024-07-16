4b5c901bfeb1b3f1aabd2e439aa957fb215f6eb7
OsuwoJr <johnfredosuwo@gmail.com>
2024-07-16 11:52:05 +0300

    Create form structure

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,29 @@ function App() {
     <div className="App">
       <form>
         <label>
-
+          First Name:
         </label>
+        <input type="text" placeholder="first name"/>
+        <label>
+          Last Name:
+        </label>
+        <input type="text" placeholder="Last name"/>
+        <label>
+          Email:
+        </label>
+        <input type="email" placeholder="email"/>
+        <label>
+          Age
+        </label>
+        <input type="number" placeholder="age"/>
+        <label>
+          Password
+        </label>
+        <input type="password" placeholder="Password"/>
+        <label>
+          Confirm Password
+        </label>
+        <input type="password" placeholder="Confirm Password"/>
       </form>
       
     </div>
