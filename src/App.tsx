93fa8a4cc30801c7f4f91c5fee4c05584d9a5a4b
OsuwoJr <johnfredosuwo@gmail.com>
2024-07-17 09:57:51 +0300

    done!

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,7 @@ function App() {
   const {
     register, 
     handleSubmit,
+    formState:{errors},
   } = useForm<FormData>({resolver:zodResolver(schema)})
 
   const submitData = (data:FormData) => {
@@ -39,26 +40,32 @@ function App() {
           First Name:
         </label>
         <input type="text" placeholder="first name" {...register("firstName")}/>
+        {errors.firstName && <span className='text-red-600'>{errors.firstName.message}</span>}
         <label>
           Last Name:
         </label>
         <input type="text" placeholder="Last name" {...register("lastName")}/>
+        {errors.lastName && <span className='text-red-600'>{errors.lastName.message}</span>}
         <label>
           Email:
         </label>
         <input type="email" placeholder="email" {...register("email")}/>
+        {errors.email && <span className='text-red-600'>{errors.email.message}</span>}
         <label>
           Age
         </label>
         <input type="number" placeholder="age" {...register("age", {valueAsNumber: true})}/>
+        {errors.age && <span className='text-red-600'>{errors.age.message}</span>}
         <label>
           Password
         </label>
         <input type="password" placeholder="Password" {...register("password")}/>
+        {errors.password && <span className='text-red-600'>{errors.password.message}</span>}
         <label>
           Confirm Password
         </label>
         <input type="password" placeholder="Confirm Password" {...register("confirmPassword")}/>
+        {errors.confirmPassword && <span className='text-red-600'>{errors.confirmPassword.message}</span>}
 
         <input type='submit'/>
       </form>
