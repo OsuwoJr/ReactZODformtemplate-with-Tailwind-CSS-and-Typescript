8b824538d61f50420d8d350a372d67e7f590d973
OsuwoJr <johnfredosuwo@gmail.com>
2024-07-17 09:08:52 +0300

    schema,formdata

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import {z} from 'zod'
-export interface IAppProps {
+import {z,ZodType} from 'zod'
+type FormData = {
+  firstName:string;
+  lastName:string;
+  email:string;
+  age:number;
+  password:string;
+  confirmPassword:string;
 }
 
 function App() {
+  const schema: ZodType<FormData> = z.object({
+    firstName:z.string().min(2).max(30),
+    lastName:z.string().min(2).max(30),
+    email: z.string().email(),
+    age:z.number().min(18).max(70),
+    password:z.string().min(5).max(20),
+    confirmPassword:z.string().min(5).max(20),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message:"Passwords do not match",
+    path:["confirmPassword"],
+  });
+
   return (
     <div className="flex items-center justify-center">
       <form className="flex flex-col w-[200px] min-h-screen">
